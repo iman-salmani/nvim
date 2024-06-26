b68acb84676f2731175e27fd36fe9a61ef1d260e
Iman Salmani <contact.imansalmani@gmail.com>
2024-06-26 20:02:16 +0330

    transparent background

--- init.lua
+++ init.lua
@@ -240,6 +240,9 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- transparent background
+  { 'xiyaowong/transparent.nvim' },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
